fc8c2727f162d00467c931696c40eb12e1973307
Eliav Hamburger <eliav.hamburger@gmail.com>
2025-05-21 03:53:23 -0400

    cleaned distance more

--- src/distance/get_depth.py
+++ src/distance/get_depth.py
@@ -2,15 +2,12 @@ import requests
 from io import BytesIO
 import numpy as np
 import cv2
+import os
 
 API_KEY = "f7375dd876b2c26a2c19680ca1aadcb8136b06ed411c88a2aec95b8e92a58e6b"
 API_URL = "https://3.231.131.94"
 API_PORT = 8000
-
-import os
-script_path = os.path.abspath(__file__)
-script_dir = os.path.dirname(script_path)
-API_CERT = (script_dir + "/cert.pem") if os.path.exists(script_dir + "/cert.pem") else "cert.pem"
+API_CERT = (os.path.dirname(os.path.abspath(__file__)) + "/cert.pem")
 
 def get_depth(img_ndarray):
     headers = {
@@ -20,11 +17,11 @@ def get_depth(img_ndarray):
 
     _, image = cv2.imencode(".jpg", img_ndarray)
     
-    files = {
+    file = {
         "image": ("frame.jpg", image, "image/jpg")
     }
 
-    response = requests.post(f"{API_URL}:{API_PORT}", headers=headers, files=files, verify=API_CERT)
+    response = requests.post(f"{API_URL}:{API_PORT}", headers=headers, files=file, verify=API_CERT)
 
     if response.status_code == 200:
         depth_data = BytesIO(response.content)
